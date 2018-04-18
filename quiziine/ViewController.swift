135682b44114961afa705d1be70b9361bb4155f2
Sheldon Lynn <sheldonlynn@outlook.com>
2018-04-18 14:28:28 -0700

    initial

---
+++ quiziine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiziine
+//
+//  Created by Sheldon Lynn on 2018-04-18.
+//  Copyright © 2018 Sheldon Lynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
